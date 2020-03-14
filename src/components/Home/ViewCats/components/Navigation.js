6bc956e934d81cca0dca7119ed716941a1afacf6
Jerson Suaza <jerson.suaza@gmail.com>
2020-03-14 12:40:14 -0500

    ui main done

--- src/components/Home/ViewCats/components/Navigation.js
+++ src/components/Home/ViewCats/components/Navigation.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { NavigationIcon } from './Navigation-icon'
+import { Couple } from './Couple'
 
 export const Navigation = () => (
     <div className="navigation">
         <div>
             <NavigationIcon name="arrow-undo" />
         </div>
-        <div className="image-couple">
-            <img src="https://www.hola.com/imagenes/estar-bien/20180926130368/como-saber-si-mi-gata-esta-en-celo/0-603-746/como-saber-si-mi-gata-esta-en-celo-ok-t.jpg" />
-            <h3>@princess</h3>
-            <p>Soy tierna, me gusta cazar pajaros y comer galletas de la suerte. Da like para que salgamos a cazar!</p>
-        </div>
+        <Couple
+            image="https://www.hola.com/imagenes/estar-bien/20180926130368/como-saber-si-mi-gata-esta-en-celo/0-603-746/como-saber-si-mi-gata-esta-en-celo-ok-t.jpg"
+            username="@princess"
+            description="Soy tierna, me gusta cazar pajaros y comer galletas de la suerte. Da like para que salgamos a cazar!"
+        />
         <div>
             <NavigationIcon name="arrow-redo" />
         </div>
--- src/components/Home/ViewCats/components/Reactions.js
+++ src/components/Home/ViewCats/components/Reactions.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { ReactionIcon } from './Reaction-icon'
 
 export const Reactions = () => (
-    <div>
-        Reations
+    <div className="reactions">
+        <ReactionIcon name="heart-dislike" />
+        <ReactionIcon color="#C800AA" name="heart" />
     </div>
 )
