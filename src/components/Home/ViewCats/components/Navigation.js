8f4f1f7dad7d03781b524e8a8143f5f440a97ff0
Jerson Suaza <jerson.suaza@gmail.com>
2020-03-14 11:37:55 -0500

    cat couple added

--- src/components/Home/ViewCats/components/Navigation.js
+++ src/components/Home/ViewCats/components/Navigation.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { NavigationIcon } from './Navigation-icon'
 
 export const Navigation = () => (
-    <div>
-        navigation
+    <div className="navigation">
+        <div>
+            <NavigationIcon name="arrow-undo" />
+        </div>
+        <div className="image-couple">
+            <img src="https://www.hola.com/imagenes/estar-bien/20180926130368/como-saber-si-mi-gata-esta-en-celo/0-603-746/como-saber-si-mi-gata-esta-en-celo-ok-t.jpg" />
+            <h3>@princess</h3>
+            <p>Soy tierna, me gusta cazar pajaros y comer galletas de la suerte. Da like para que salgamos a cazar!</p>
+        </div>
+        <div>
+            <NavigationIcon name="arrow-redo" />
+        </div>
     </div>
 )
