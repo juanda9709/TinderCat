d2ec7ccebd0741ff544e87f21be27c66f1cb057f
Jerson Suaza <jerson.suaza@gmail.com>
2020-03-14 10:13:11 -0500

    view cats updated

---
+++ src/components/Home/ViewCats/components/Reactions.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Reactions = () => (
+    <div>
+        Reations
+    </div>
+)
---
+++ src/components/Home/ViewCats/components/Topbar-icon.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const TopbarIcon = ({ name, badge }) => (
+    <div className="topbar-icon">
+        <ion-icon name={ name }></ion-icon>
+        {/*badge ? <span>{ badge }</span> : null */}
+        { badge && <span>{ badge }</span> }
+    </div>
+)
